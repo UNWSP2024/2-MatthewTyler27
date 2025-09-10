6c8b1ac6ea78e931512a7a3632fabcda3bb8e3ff
MatthewTyler27 <mltyler@students.unwsp.edu>
2025-09-10 18:34:59 -0500

    Assignment_1.py

--- program_1.py
+++ program_1.py
@@ -1,6 +1,12 @@
+# Matthew Tyler
+# 09/10/25
+# Personal Information
 def personal_information():
-    print('...')
-
+    # Information below in print statements
+    print('Matthew Tyler')
+    print('Vinland, Minnesota 54167')
+    print('952-741-9067')
+    print('Mechanical Engineering')
 
 # Line which calls the above function.
 personal_information()
